35c9d46230a8e0a343127016cfd93fdaa8fc0398
Forrest Holub <forrestdholub@gmail.com>
2021-01-11 02:41:46 -0800

    modified logic for different cases for each oem. broke it out more cleanly

--- add_inventory.bat
+++ add_inventory.bat
@@ -28,8 +28,8 @@ IF "%serial%"==""  ECHO Device not connected correctly. Please try authorizing A
 
 ::setup excel sheet for first time 
 IF EXIST Device_Inventory.csv SET firstentry=0
-IF NOT "%firstentry%"=="0" ECHO Manufacturer,Model,Codename,Model Number,Carrier,IMEI,Serial Number,Barcode,HW,ICCID,IMSI,MDN,Owner,Custody,Received,Transfer Dates,Additional info>>Device_Inventory.csv
-
+IF NOT "%firstentry%"=="0" ECHO Manufacturer,Model,Codename,Model Number,Carrier,IMEI,Serial Number,Barcode,HW,ICCID,IMSI,MDN,Custody,Date,Owner,Transfer Date,Additional info>>Device_Inventory.csv
+REM ---FOR -------------REFERENCE--   %brand%,%model%,%codename%,%sku%,%carrier%,'%imei%',%serial%,%barcode%,%hardware%,'%iccid%','%imsi%','%mdn%',%custody%,%date%,%owner%,%transfer%,%comments%
 IF "%firstentry%"=="0" (
     GOTO :check_INVENTORY
 )
@@ -99,51 +99,58 @@ REM DON'T KNOW! DON'T CARE?
 REM HW (DVT or PVT) __CURRENTLY BEST WAY TO DETERMINE HW____
 FOR /f "delims=" %%A in ('adb shell getprop ro.vendor.hw.revision') do SET hardware=%%A
 
-REM ICCID ----11 (ICCID)-moto?
+REM ICCID ----11 (ICCID)-moto
 FOR /F "skip=1 tokens=2,3,4,5,6,7,8,9 delims='.)" %%i IN ('adb shell service call iphonesubinfo 11') DO <NUL SET /P result=%%i%%j%%k%%l%%m%%n%%o%%p>>temp.txt
 IF EXIST temp.txt FOR /F "tokens=1 delims= " %%i IN (temp.txt) DO SET iccid1=%%i
 IF EXIST temp.txt del temp.txt 2>&1>NUL
 
-REM ICCID ----12 (ICCID) -samsung?
+REM ICCID ----12 (ICCID)-google
 FOR /F "skip=1 tokens=2,3,4,5,6,7,8,9 delims='.)" %%i IN ('adb shell service call iphonesubinfo 12') DO <NUL SET /P result=%%i%%j%%k%%l%%m%%n%%o%%p>>temp.txt
 IF EXIST temp.txt FOR /F "tokens=1 delims= " %%i IN (temp.txt) DO SET iccid2=%%i
 IF EXIST temp.txt del temp.txt 2>&1>NUL
 
-REM SET ICCID CORRECTLY
-IF "%ICCID1%"=="" (SET iccid=%ICCID2%
-)ELSE ( SET iccid=%ICCID1%)
+REM ICCID ----13 (ICCID)-samsung
+FOR /F "skip=1 tokens=2,3,4,5,6,7,8,9 delims='.)" %%i IN ('adb shell service call iphonesubinfo 13') DO <NUL SET /P result=%%i%%j%%k%%l%%m%%n%%o%%p>>temp.txt
+IF EXIST temp.txt FOR /F "tokens=1 delims= " %%i IN (temp.txt) DO SET iccid3=%%i
+IF EXIST temp.txt del temp.txt 2>&1>NUL
 
-REM IMSI -----iphonesubinfo 7 (IMSI)
+REM IMSI -----iphonesubinfo 7 (IMSI)-lge
 FOR /F "skip=1 tokens=2,3,4,5,6,7,8,9 delims='.)" %%i IN ('adb shell service call iphonesubinfo 7') DO <NUL SET /P result=%%i%%j%%k%%l%%m%%n%%o%%p>>temp.txt
 IF EXIST temp.txt FOR /F "tokens=1 delims= " %%i IN (temp.txt) DO SET imsi=%%i
 IF EXIST temp.txt del temp.txt 2>&1>NUL
 
-REM : MDN ----iphonesubinfo 17 (MDN)-moto?
+REM : MDN ----iphonesubinfo 17 (MDN)-moto, samsung
 FOR /F "skip=1 tokens=2,3,4,5,6,7,8,9 delims='.)" %%i IN ('adb shell service call iphonesubinfo 17') DO <NUL SET /P result=%%i%%j%%k%%l%%m%%n%%o%%p>>temp.txt
 IF EXIST temp.txt FOR /F "tokens=1 delims= " %%i IN (temp.txt) DO SET mdn1=%%i
 IF EXIST temp.txt del temp.txt 2>&1>NUL
 
-REM : MDN ----iphonesubinfo 18 (MDN)-samsung?
+REM : MDN ----iphonesubinfo 18 (MDN)-lge
 FOR /F "skip=1 tokens=2,3,4,5,6,7,8,9 delims='.)" %%i IN ('adb shell service call iphonesubinfo 18') DO <NUL SET /P result=%%i%%j%%k%%l%%m%%n%%o%%p>>temp.txt
 IF EXIST temp.txt FOR /F "tokens=1 delims= " %%i IN (temp.txt) DO SET mdn2=%%i
 IF EXIST temp.txt del temp.txt 2>&1>NUL
 
-REM SET MDN CORRECTLY
-IF "%mdn1%"=="" (SET mdn=%mdn2%
-)ELSE ( SET mdn=%mdn1%)
+REM : MDN ----iphonesubinfo 19 (MDN)-google
+FOR /F "skip=1 tokens=2,3,4,5,6,7,8,9 delims='.)" %%i IN ('adb shell service call iphonesubinfo 19') DO <NUL SET /P result=%%i%%j%%k%%l%%m%%n%%o%%p>>temp.txt
+IF EXIST temp.txt FOR /F "tokens=1 delims= " %%i IN (temp.txt) DO SET mdn3=%%i
+IF EXIST temp.txt del temp.txt 2>&1>NUL
+
+
+REM change nuences by manufacture
+IF "%brand%"=="google" SET "iccid=%iccid2%"
+IF "%brand%"=="google" SET "mdn=%mdn3%"
+
+IF "%brand%"=="samsung" SET "iccid=%iccid3%"
+IF "%brand%"=="samsung" SET "mdn=%mdn1%"
+
+IF "%brand%"=="motorola" SET "iccid=%iccid1%"
+IF "%brand%"=="motorola" SET "mdn=%mdn1%"
+
+REM STILL NEED TO ADD:::::
+IF "%brand%"=="lge" SET "iccid="
+IF "%brand%"=="lge" SET "mdn="
 
 ECHO. 
-ECHO. [1] ARCLIGHT [REF]
-ECHO. [2] T-MOBILE [REF]
-ECHO. [3] SPRINT   [REF]
-ECHO. [4] DUT      [DUT]
-
-CHOICE /C 1234 /T 5 /D 4 /N /M "Who is owner?"
-IF %ERRORLEVEL% EQU 1 SET owner=arclight (REF)
-IF %ERRORLEVEL% EQU 2 SET owner=t-mobile (REF)
-IF %ERRORLEVEL% EQU 3 SET owner=sprint (REF)
-IF %ERRORLEVEL% EQU 4 SET owner=DUT
-
+ECHO. 
 CHOICE /C yn /T 5 /D n /N /M "Add additional info?: [y]for YES [n]for NO. You have [5]seconds to make up your mind"
 IF %ERRORLEVEL% EQU 1 GOTO additional_info
 IF %ERRORLEVEL% EQU 2 GOTO output
@@ -151,21 +158,30 @@ IF %ERRORLEVEL% EQU 2 GOTO output
 :additional_info
 REM 7: Barcode
 SET /p barcode="7: Additional Barcode:"
-
 REM 11: Custody
 SET /p custody="11: Enter Custody:"
 REM 13: Transfers
 SET /p transfer="13: Enter transfer date:"
 REM 14: ADDITIONAL INFORMATION
 SET /p comments="14: Additional comments:"
-4
 
-REM FUTURE EXTRAS:?:
-REM iphonesubinfo 28 (SIP_ID)
+ECHO. 
+ECHO. 
+ECHO. [1] ARCLIGHT [REF]
+ECHO. [2] T-MOBILE [REF]
+ECHO. [3] SPRINT   [REF]
+ECHO. [4] DUT      [DUT]
+
+CHOICE /C 1234 /T 4 /D 4 /N /M "Who is owner?"
+IF %ERRORLEVEL% EQU 1 SET owner=arclight (REF)
+IF %ERRORLEVEL% EQU 2 SET owner=t-mobile (REF)
+IF %ERRORLEVEL% EQU 3 SET owner=sprint (REF)
+IF %ERRORLEVEL% EQU 4 SET owner=DUT
+
 REM whoami => auto list custody -> parse : desktop-34pmf9p\forre
 
 :output
-ECHO %brand%,%model%,%codename%,%sku%,%carrier%,%imei%,%serial%,%barcode%,%hardware%,%iccid%,%imsi%,%mdn%,%owner%,%custody%,%date%,%transfer%,%comments%>>Device_Inventory.csv
+ECHO %brand%,%model%,%codename%,%sku%,%carrier%,'%imei%',%serial%,%barcode%,%hardware%,'%iccid%','%imsi%','%mdn%',%custody%,%date%,%owner%,%transfer%,%comments%>>Device_Inventory.csv
 
 
 ENDLOCAL
